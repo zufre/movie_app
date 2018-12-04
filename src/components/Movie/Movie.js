346c977a3cf2ed08ceaed84ccc7e2d90b7c5a4de
Cédric David <cedric.david@btopenworld.com>
2018-12-04 12:32:36 +1000

    add actor movieinfo  movieInfobar navigation

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,8 +1,71 @@
 import React, { Component } from "react";
+import { API_URL, API_KEY } from "../../config";
+import Navigation from "../elements/Navigation/Navigation";
+import MovieInfo from "../elements/MovieInfo/MovieInfo";
+import MovieInfoBar from "../elements/MovieInfoBar/MovieInfoBar";
+import FourColGrid from "../elements/FourColGrid/FourColGrid";
+import Actor from "../elements/Actor/Actor";
+import Spinner from "../elements/Spinner/Spinner";
+import "./Movie.css";
 
 class Movie extends Component {
+  state = {
+    movie: null,
+    actors: null,
+    directors: [],
+    loading: false
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    const endpoint = `${API_URL}movie/${
+      this.props.match.params.movieId
+    }?api_key=${API_KEY}&language=e-US`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(result =>
+        result.json().then(result => {
+          console.log(result);
+          if (result.status_code) {
+            this.setState({ loading: false });
+          } else {
+            this.setState({ movie: result }, () => {
+              const endpoint = `${API_URL}movie/${
+                this.props.match.params.movieId
+              }/credits?api_key=${API_KEY}`;
+              fetch(endpoint)
+                .then(result => result.json())
+                .then(result => {
+                  const directors = result.crew.filter(
+                    member => member.job === "Director"
+                  );
+
+                  this.setState({
+                    actors: result.cast,
+                    directors,
+                    loading: false
+                  });
+                });
+            });
+          }
+        })
+      )
+      .catch(error => console.error("Error", error));
+  };
   render() {
-    return <div>Movie</div>;
+    return (
+      <div className="rmdb-movie">
+        <Navigation />
+        <MovieInfo />
+        <MovieInfoBar />
+        {/* <FourColGrid /> */}
+        <Spinner />
+      </div>
+    );
   }
 }
 export default Movie;
---
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Fontawesome from "react-fontawesome";
+import { clacTime, convertMoney } from "../../../helpers.js";
+import "./MovieInfoBar.css";
+
+const MovieInfoBar = props => {
+  return <div>MovieInfoBar</div>;
+};
+
+export default MovieInfoBar;
