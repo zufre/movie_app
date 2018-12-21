c25fc79961c457cc66bdb0f264608fa931660f66
Cédric David <cedric.david@btopenworld.com>
2018-12-21 10:11:04 +1000

    update files

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -6,18 +6,11 @@ const Header = () => {
   return (
     <div className="rmdb-header">
       <div className="rmdb-header-content">
-        <Link to="/">
-          <img
-            className="rmdb-logo"
-            src="./images/reactMovie_logo.png"
-            alt="rmdb-logo"
-          />
+        <Link style={{ textDecoration: "none" }} to="/">
+          <h1>
+            Movie <i>Browser</i>
+          </h1>
         </Link>
-        <img
-          className="rmdb-tmdb-logo"
-          src="./images/tmdb_logo.png"
-          alt="tmdb-logo"
-        />
       </div>
     </div>
   );
